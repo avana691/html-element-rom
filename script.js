79fcaed7a9d83bf5fdf070fdb3733da11a2efe92
avana691 <avana691@newschool.edu>
2026-03-26 09:33:50 -0400

    add new

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const containers = document.querySelectorAll(
-    ".audio1, .audio2, .audio3, .audio4, .audio5"
+    ".audio1, .audio2, .audio3, .audio4, .audio5, .audio6, .audio7, .audio8, .audio9, .audio10, .audio11, .audio12, .audio13, .audio14, .audio15, .audio16, .audio17, .audio18, .audio19, .audio20"
   );
 
   containers.forEach(container => {
 
-    const albumImg = container.querySelector(".album-img");
+    const recordImg = container.querySelector(".record-img");
     const audio = container.querySelector("audio");
 
-    // Click → hide album + play audio
-    albumImg.addEventListener("click", () => {
+    container.addEventListener("click", () => {
 
-      // Stop all others
+      // stop others
       containers.forEach(c => {
         const a = c.querySelector("audio");
-        const img = c.querySelector(".album-img");
+        const rec = c.querySelector(".record-img");
 
         if (a !== audio) {
           a.pause();
           a.currentTime = 0;
-          img.classList.remove("hidden");
+          rec?.classList.remove("playing");
         }
       });
 
-      // Hide clicked album
-      albumImg.classList.add("hidden");
+      // toggle play
+      if (audio.paused) {
+        audio.play();
+        recordImg.classList.add("playing");
+      } else {
+        audio.pause();
+        recordImg.classList.remove("playing");
+      }
 
-      // Play audio
-      audio.play();
     });
 
   });
